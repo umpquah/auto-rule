8e736fed341c80da88b3b914f92440590ce22812
Colin Schatz <schatz@amyris.com>
2024-10-26 20:31:16 -0700

    Add dependencies, remove boilerplate

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+
+import { Container, Tab, Tabs } from "react-bootstrap";
+import '../style.scss';
+
+function App() {
+  return (
+    <Container id="app">
+      [Placeholder - working on model currently]
+    </Container>
+  )
+}
+
+export default App;
+
+
